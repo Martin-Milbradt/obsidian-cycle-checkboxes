5f1ea27467fff90ee40357db610eeb55a2ed3938
Milli <onethenumbernottheword@gmail.com>
2023-11-30 16:05:30 +0100

    initial

--- main.ts
+++ main.ts
@@ -1,89 +1,122 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting, debounce } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CycleSettings {
+	fillings: string[];
+	reload: boolean;
+	includeEmpty: boolean;
+	includeDash: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: CycleSettings = {
+	fillings: [" ", "x", "/", "-"],
+	reload: true,
+	includeEmpty: true,
+	includeDash: true,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CycleCheckboxes extends Plugin {
+	settings: CycleSettings;
+
+	prefixes: string[];
+	prefixRegEx: string;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'cycle-forward',
+			name: 'Cycle forward',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (!view) {
+					return;
+				} else {
+					const view_mode = view.getMode();
+				switch (view_mode) {
+						case "preview":
+							return;
+						case "source": {
+							this.cycle(view.editor);
+						}
 					}
+				}
+			}
+		});
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.addCommand({
+			id: 'cycle-backward',
+			name: 'Cycle backward',
+			callback: () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (!view) {
+					return;
+				} else {
+					const view_mode = view.getMode();
+				switch (view_mode) {
+						case "preview":
+							return;
+						case "source": {
+							this.cycle(view.editor, -1);
+						}
+					}
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new CycleSettingsTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
+		// Intentionally empty
+	}
 
+	cycle(editor: Editor, offset = 1) {
+		if (this.settings.reload) {
+			// Create variables for prefix matching
+			this.prefixes = [...this.settings.fillings.map(e => `- [${e}] `)];
+			let regExPart = this.settings.fillings.map(e => `- \\[\\${e}\\] `).join("|");
+			if (this.settings.includeDash) {
+				this.prefixes.push("- ");
+				regExPart += "|- ";
+			}
+			if (this.settings.includeEmpty) {
+				this.prefixes.push("");
+				regExPart += "|";
+			}
+			this.prefixRegEx = `^(\\s*)(${regExPart}|- \\[.\\]|- |)(.*)$`;
+			this.settings.reload = false;
+		}
+		const start = editor.getCursor("from")
+		const end = editor.getCursor("to")
+		for (let i = start.line; i <= end.line; i++) {
+			const lineContents = editor.getLine(i);
+			console.log(i + ": " + lineContents)
+			const match = RegExp(this.prefixRegEx).exec(lineContents);
+			if (!match) {
+				console.log("no match for line " + i + ": " + lineContents);
+				return;
+			}
+			const prefix = match[2];
+			const currentPrefixIndex = this.prefixes.findIndex(p => p == prefix); // not found defaults to -1
+			const nextPrefix = this.prefixes[((currentPrefixIndex + offset) + this.prefixes.length) % this.prefixes.length]; // + length to avoid negative modulo
+			const newLineContent = match[1] + nextPrefix + match[3]
+			editor.setLine(i, newLineContent);
+			if (i == start.line) {
+				start.ch +=  nextPrefix.length - prefix.length;
+			}
+			if (i == end.line) {
+				end.ch +=  nextPrefix.length - prefix.length;
+			}
+		}
+		editor.setSelection(start, end);
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = { ...DEFAULT_SETTINGS, ...await this.loadData()};
 	}
 
 	async saveSettings() {
@@ -91,44 +124,61 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CycleSettingsTab extends PluginSettingTab {
+	plugin: CycleCheckboxes;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CycleCheckboxes) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
-
+	fillings = [" ", "x", "/", "-"];
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
+		containerEl.createEl('h2', { text: 'Cycle Checkboxes' });
+
+		new Setting(this.containerEl)
+            .setName("Enable bullet point")
+            .setDesc("Adds the generic bullet point '-' to the cycle.")
+            .addToggle(toggle =>
+                toggle
+                    .setValue(this.plugin.settings.includeDash)
+                    .onChange(async () => {
+						this.plugin.settings.includeDash = !this.plugin.settings.includeDash;
+						this.plugin.settings.reload = true;
+						await this.plugin.saveSettings()
+					})
+            );
+
+		new Setting(this.containerEl)
+            .setName("Enable no checkbox")
+            .setDesc("Adds removal of checkboxes to the cycle.")
+            .addToggle(toggle =>
+                toggle
+                    .setValue(this.plugin.settings.includeEmpty)
+                    .onChange(async () => {
+						this.plugin.settings.includeEmpty = !this.plugin.settings.includeEmpty;
+						this.plugin.settings.reload = true;
+						await this.plugin.saveSettings()
+					})
+            );
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Ckeckboxes to be cycled')
+			.setDesc('Enter the checkboxes you want to toggle in a comma-separated list. Note: The empty checkbox has to be included as well. Restart Obsidian after changing this setting.')
+			.addTextArea(text => {
+                const onChange = async (value: string) => {
+                    const list = value.split(',').map((v) => v.trim() || ' ');
+                    this.plugin.settings.fillings = list;
+					this.plugin.settings.reload = true;
+                    await this.plugin.saveSettings();
+                };
+                text.setPlaceholder(
+                    'List checkboxes to cycle',
+                );
+                text.setValue(
+                    this.plugin.settings.fillings.join(', '),
+                ).onChange(debounce(onChange, 500, true));
+            });
 	}
 }
